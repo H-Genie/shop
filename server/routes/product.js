4700b029e12b21874749eedb869d6c50aae9491b
h-genie <ses2000hj@hanmail.net>
2021-12-25 22:34:28 +0900

    1-5. 이미지 파일을 서버로 보내기

---
+++ server/routes/product.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+//=================================
+//             Product
+//=================================
+
+router.post('/image', (req, res) => {
+
+})
+
+module.exports = router;
