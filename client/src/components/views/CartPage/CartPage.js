0ffca456ff72a68ff439e1ecb4f24168d9689fcb
H-Genie <61546294+H-Genie@users.noreply.github.com>
2022-01-19 15:46:44 +0900

    4-6. 카트에 들어있는 상품들 가격 계산

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { getCartItems } from '../../../_actions/user_actions'
 import UserCardBlock from './Sections/UserCardBlock'
 
 function CartPage(props) {
     const dispatch = useDispatch()
+    const [total, setTotal] = useState(0)
 
     useEffect(() => {
         let cartItems = []
@@ -16,14 +17,32 @@ function CartPage(props) {
                 })
 
                 dispatch(getCartItems(cartItems, props.user.userData.cart))
+                    .then(response => {
+                        calculateTotal(response.payload)
+                    })
             }
         }
     }, [props.user.userData])
 
+    let calculateTotal = cartDetail => {
+        let total = 0;
+        cartDetail.map(item => {
+            total += parseInt(item.price, 10) * item.quantity
+        })
+        setTotal(total)
+    }
+
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
             <h1>My Cart</h1>
-            <UserCardBlock products={props.user.cartDetail} />
+
+            <div>
+                <UserCardBlock products={props.user.cartDetail} />
+            </div>
+
+            <div style={{ marginTop: '3rem' }}>
+                <h2>Total Amount : ${total}</h2>
+            </div>
         </div>
     )
 }
