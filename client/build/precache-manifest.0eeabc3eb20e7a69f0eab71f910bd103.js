273a3aef41e93e14a2d5586b1ca4b1eb6bf9a7a2
H-Genie <61546294+H-Genie@users.noreply.github.com>
2022-01-24 10:02:33 +0900

    deploy

---
+++ client/build/precache-manifest.0eeabc3eb20e7a69f0eab71f910bd103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e2f5a96c7d0387d2ee52f081ef3990",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78ca12ad125bc1f801b1",
+    "url": "/static/css/2.1badfcbc.chunk.css"
+  },
+  {
+    "revision": "651e211934d2ab491dbd",
+    "url": "/static/css/main.eea35913.chunk.css"
+  },
+  {
+    "revision": "78ca12ad125bc1f801b1",
+    "url": "/static/js/2.d4428205.chunk.js"
+  },
+  {
+    "revision": "5eaa82700f8bcfbbdd8e24e620f849e7",
+    "url": "/static/js/2.d4428205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "651e211934d2ab491dbd",
+    "url": "/static/js/main.33ee21b4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
